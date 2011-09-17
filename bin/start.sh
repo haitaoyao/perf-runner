3dd122c1545e3635ddded5db44d8797f634e65aa
haitao.yao <yao.erix@gmail.com>
2011-09-17 09:49:57 +0800

    add files

---
+++ bin/start.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+##################################################################
+# 
+# written by haitao.yao @ 2011-09-16.23:47:31
+# 
+# this is used to start the perf test and gather the basic perf data
+# 
+##################################################################
+current_dir="$(cd $(dirname $0);pwd)"
+PERF_RUNNER_HOME="$(cd $(dirname $0)/../;pwd)"
+PERF_RUNNER_DEPLOY_FOLDER=$PERF_RUNNER_HOME/deploy
+# print the help information
+function print_help()
+{
+	echo "perf-runner start script"
+	echo "Usage: $0 -n perf_test_name"
+	echo
+	printf "\tperf_test_name\t the perf test name deployed in the deploy folder\n"
+	echo
+
+}	
+
+if [ -z "$1" ]
+then
+	print_help
+	exit 1
+fi
+
+#parse the arguments
+while getopts ":n:" OPT
+do
+	case $OPT in
+		n)
+			perf_test_name=$OPTARG
+			;;
+		?)
+			print_help
+			exit 1
+			;;
+		:)
+			print_help
+			exit 1
+			;;
+	esac
+
+done
+if [ ! -d $PERF_RUNNER_DEPLOY_FOLDER/$perf_test_name ]
+then
+	# no perf test in deploy folder, exit 
+	echo "no perf test : $perf_test_name in $PERF_RUNNER_DEPLOY_FOLDER"
+	exit 1
+fi
