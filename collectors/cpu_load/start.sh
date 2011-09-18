2b9fc28da7fc8885d956d6cabd086c84928eb202
haitao.yao <yao.erix@gmail.com>
2011-09-18 13:21:41 +0800

    change interval to 5

--- collectors/cpu_load/start.sh
+++ collectors/cpu_load/start.sh
@@ -10,11 +10,11 @@ current_dir="$(cd $(dirname $0);pwd)"
 
 get_cpu_load()
 {
-	echo "timestamp,load_1,load_5,load15,"
+	echo "timestamp,load_1,load_5,load_15,"
 	while : 
 	do
 		echo "$(date +%Y%m%d%H%M%S)$(w |grep 'load averages:'|awk -F 'load averages:' '{print $2}'|sed 's/ /,/g')"
-		sleep 1
+		sleep 5 
 	done
 }
 
