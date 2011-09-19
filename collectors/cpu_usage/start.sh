6e43679257b2bfc77f6014cb07f38cb63e3b5b88
haitao.yao <yao.erix@gmail.com>
2011-09-19 21:40:49 +0800

    add cpu_usage

---
+++ collectors/cpu_usage/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+##################################################################
+# 
+# written by haitao.yao @ 2011-09-19.20:09:09
+# 
+# used to collect cpu usage info 
+# 
+##################################################################
+current_dir="$(cd $(dirname $0);pwd)"
+
+wait_interval=5
+echo "#timestamp, %usr, %nice, %sys, %iowait, %irq, %soft, %steal, %guest, %idle"
+while :
+do
+	sleep $wait_interval
+	echo "$(date +%Y%m%d%H%M%S)$(mpstat |grep all|awk -F 'all' '{print $2}'|sed 's/ * /,/g')"
+done
