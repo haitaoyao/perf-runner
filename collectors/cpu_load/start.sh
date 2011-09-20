232e9cbf6b4f1005bb8911b56e5066a7dee31f0f
haitao.yao <yao.erix@gmail.com>
2011-09-20 09:50:15 +0800

    fix for load

--- collectors/cpu_load/start.sh
+++ collectors/cpu_load/start.sh
@@ -13,7 +13,7 @@ get_cpu_load()
 	echo "#timestamp,load_1,load_5,load_15,"
 	while : 
 	do
-		echo "$(date +%Y%m%d%H%M%S)$(w |grep 'load average'|awk -F 'load average' '{print $2}'|sed 's/ /,/g')"
+		echo "$(date +%Y%m%d%H%M%S),$(w |grep 'load average:'|awk -F 'load average:' '{print $2}')"
 		sleep 5 
 	done
 }
