c9aed56fd8b2a6dcae762404db96360da0afd128
haitao.yao <yao.erix@gmail.com>
2012-07-12 22:57:09 +0800

    bug fix for network interface

--- collectors/network/start.sh
+++ collectors/network/start.sh
@@ -18,7 +18,7 @@ net_value=$(($wait_interval * 1024 ))
 while :
 do
 	sleep $wait_interval
-	net_info=$(ifconfig eth0|grep bytes)
+	net_info=$(ifconfig $network_interface|grep bytes)
 	now_in=$(echo $net_info|awk -F 'bytes:' '{print $2}'|cut -d ' ' -f 1)
 	now_out=$(echo $net_info|awk -F 'bytes:' '{print $3}'|cut -d ' ' -f 1)
 	in_speed=$(expr $(expr $now_in - $last_in ) / $net_value)
